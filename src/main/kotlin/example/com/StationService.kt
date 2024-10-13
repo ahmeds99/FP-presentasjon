9ef7b96599bed5ff86c015c6430e25503017cf20
ahmeds99 <ahmed99@live.no>
2024-10-13 22:31:38 +0200

    add endpoints with nullability and exceptions

--- src/main/kotlin/example/com/StationService.kt
+++ src/main/kotlin/example/com/StationService.kt
@@ -4,14 +4,49 @@ import example.com.models.Station
 import example.com.models.StationInfoResponse
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.http.isSuccess
 
 class StationService {
     val stationInfoURL = "https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json"
-
+    
     suspend fun getStations(): List<Station> {
         val response = client.get(stationInfoURL)
 
+        if (!response.status.isSuccess()) {
+            throw Exception("Feil ved henting av data: ${response.status}")
+        }
+
         val responseBody = response.body<StationInfoResponse>()
+        val allStations = responseBody.data.stations
+
+        check(allStations.isNotEmpty()) { "Fikk ut tom liste.." }
+
         return responseBody.data.stations
+    }
+
+    suspend fun getStationNullable(stationId: String): Station? {
+        if (stationId.isEmpty()) {
+            return null
+        }
+
+        try {
+            val allStations = getStations()
+            val station = allStations.find { station ->
+                station.id == stationId
+            }
+
+            if (station == null) {
+                println("Could not find station...")
+                return null
+            }
+
+            return station
+        } catch (e: IllegalStateException) {
+            println("Feil: ${e.message}")
+            return null
+        } catch (e: Exception) {
+            println("Fikk feil grunnet nettverksfeil ${e.message}")
+            return null
+        }
     }
 }
--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package example.com.plugins
 
 import example.com.stationService
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -13,6 +14,18 @@ fun Application.configureRouting() {
 
         get("/stations") {
             call.respond(stationService.getStations())
+        }
+
+        get("/station/{id}") {
+            val id = call.parameters["id"]
+            if (id != null) {
+                val station = stationService.getStationNullable(id)
+                if (station != null) {
+                    call.respond(station)
+                } else {
+                    call.respond(HttpStatusCode.NotFound, "Station not found!")
+                }
+            }
         }
     }
 }
